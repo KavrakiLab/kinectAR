f43458a7618e6a3dce156e1cd9243b24f07e1fd0
Neil T. Dantam <ntd@gatech.edu>
2014-07-07 21:44:01 -0400

    Separate file for ImageReceiver

---
+++ src/imageReceiver.cpp
@@ -0,0 +1,64 @@
+/* -*- mode: C++; c-basic-offset: 8; indent-tabs-mode: t;  -*- */
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <time.h>
+#include <stdint.h>
+#include <ach.h>
+#include <sns.h>
+#include "imageReceiver.h"
+
+void ImageReceiver::init(const char* channelName, int resX, int resY )
+{
+
+	sns_init();
+	int r = ach_open(&chan, channelName, NULL);
+	assert( ACH_OK == r );
+
+
+	iWidth  = resX;
+	iHeight = resY;
+
+	size = sizeof(frame) + sizeof(frame->points[0])*(iWidth*iHeight*3);
+	frame = (frame_t*) (malloc(size));
+	frame->width = iWidth;
+	frame->height = iHeight;
+	initialized = true;
+}
+
+Mat ImageReceiver::receiveImage()
+{
+	Mat image(frame->width, frame->height, CV_8UC3);
+	if(!initialized) return image;
+
+	size_t fs;
+	//std::cout << "Pull message .." << std::endl;
+	ach_status_t r = ach_get( &chan, frame, size, &fs,
+				  NULL, (ach_get_opts_t)(ACH_O_LAST) );
+
+	//fprintf(stderr, "result: %s\n", ach_result_to_string(r));
+	//assert( ACH_OK == r || ACH_MISSED_FRAME == r);
+
+	int n = frame->width * frame->height;
+
+	//assert( n < fs / sizeof(frame->points[0][0]) );
+	//std::cout << frame->width << " " << frame->height << std::endl;
+
+	int i = 0;
+	for(int x = 0; x < frame->width; x++)
+	{
+		for(int y = 0; y < frame->height; y++)
+		{
+			int r = (int)frame->points[i][0];
+			int g = (int)frame->points[i][1];
+			int b = (int)frame->points[i][2];
+			//std::cout << r << " " << g << " " << b << std::endl;
+			assert(i < frame->width*frame->height);
+			image.at<Vec3b>(x,y)[0] = r;
+			image.at<Vec3b>(x,y)[1] = g;
+			image.at<Vec3b>(x,y)[2] = b;
+			i++;
+		}
+	}
+	return image;
+}
