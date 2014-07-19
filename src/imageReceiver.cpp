e14cee95335435b712ac034b649bebd508f307bf
Neil T. Dantam <ntd@gatech.edu>
2014-07-19 03:13:05 -0400

    Image display

--- src/imageReceiver.cpp
+++ src/imageReceiver.cpp
@@ -35,31 +35,27 @@ Mat *ImageReceiver::receiveImage(Mat *m)
 			     m->cols == frame->height,
 			     "Size mismatch on receiving frame as matrix\n" );
 	} else  {
-		m = new Mat(frame->width, frame->height, CV_8UC3);
-	}
-	int n = frame->width * frame->height;
 
+		m = new Mat(frame->height, frame->width, CV_8UC3, Scalar(0,0,255));
+		//m = new Mat(frame->width, frame->height, CV_8UC3);
+	}
 	// TODO: check than frame size is reasonable
 
-	//assert( n < fs / sizeof(frame->points[0][0]) );
-	//std::cout << frame->width << " " << frame->height << std::endl;
-
-	int i = 0;
-	for(int x = 0; x < frame->width; x++)
+	int k = 0;
+	for(int i = 0; i < frame->height; i++)
 	{
-		for(int y = 0; y < frame->height; y++)
+		for(int j = 0; j < frame->width; j++)
 		{
-			int r = (int)frame->points[i][0];
-			int g = (int)frame->points[i][1];
-			int b = (int)frame->points[i][2];
-			//std::cout << r << " " << g << " " << b << std::endl;
+			int r = (int)frame->points[k][0];
+			int g = (int)frame->points[k][1];
+			int b = (int)frame->points[k][2];
+
 			assert(i < frame->width*frame->height);
-			m->at<Vec3b>(x,y)[0] = r;
-			m->at<Vec3b>(x,y)[1] = g;
-			m->at<Vec3b>(x,y)[2] = b;
-			i++;
+			m->at<Vec3b>(i,j)[0] = r;
+			m->at<Vec3b>(i,j)[1] = g;
+			m->at<Vec3b>(i,j)[2] = b;
+			k++;
 		}
 	}
-
 	return m;
 }
---
+++ src/receive.cpp
@@ -0,0 +1,42 @@
+/* -*- mode: C++; c-basic-offset: 8; indent-tabs-mode: t;  -*- */
+#include <amino.h>
+#include <ach.h>
+#include <sns.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include <iostream>
+#include <sns.h>
+
+#include "imageReceiver.h"
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+	sns_init();
+	if(argc < 2)
+	{
+		std::cout << "Usage: ./receive channelname" << std::endl;
+		return 0;
+	}
+
+	ImageReceiver rec(argv[1]);
+
+	sns_start();
+	while(!sns_cx.shutdown)
+	{
+		Mat *m = rec.receiveImage(NULL);
+		if(m) {
+			imshow( "receive", *m );
+			delete m;
+		}
+
+		waitKey( 30 );
+
+		aa_mem_region_local_release();
+	}
+	return 0;
+}
