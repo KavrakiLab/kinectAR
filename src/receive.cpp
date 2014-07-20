8ef42f8177ec70b9ad89a0837faefa0d0131120f
Neil T. Dantam <ntd@gatech.edu>
2014-07-19 23:40:24 -0400

    Call ach_cancel in imshow

--- src/receive.cpp
+++ src/receive.cpp
@@ -11,24 +11,26 @@
 
 #include "imageReceiver.h"
 
-using namespace cv;
-using namespace std;
-
 int main( int argc, char* argv[] )
 {
 	sns_init();
 	if(argc < 2)
 	{
-		std::cout << "Usage: ./receive channelname" << std::endl;
+		std::cout << "Usage: imshow channel" << std::endl;
 		return 0;
 	}
 
 	ImageReceiver rec(argv[1]);
 
+	{
+		ach_channel_t *chans[] = {&rec.chan, NULL};
+		sns_sigcancel( chans, sns_sig_term_default );
+	}
+
 	sns_start();
 	while(!sns_cx.shutdown)
 	{
-		Mat *m = rec.receiveImage2(NULL);
+		cv::Mat *m = rec.receiveImage2(NULL);
 		if(m) {
 			imshow( "receive", *m );
 			delete m;
